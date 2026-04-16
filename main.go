4af5016fab5a9ed19423e392723378bb1dcef66a
Karim OCB <chaya.karim.omar@gmail.com>
2026-04-16 01:09:05 +0000

    AC: REPL Testing

--- main.go
+++ main.go
@@ -1,9 +1,64 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 )
 
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
 func main() {
-	fmt.Println("Hello, World!")
+	scanner := bufio.NewScanner(os.Stdin)
+	registry := getCommand()
+
+	for {
+		fmt.Printf("Pokedex > ")
+		scanner.Scan()
+		line := scanner.Text()
+		words := cleanInput(line)
+
+		val, ok := registry[words[0]]
+		if ok {
+			val.callback()
+		} else {
+			fmt.Println("Unknown command")
+		}
+	}
+}
+
+func getCommand() map[string]cliCommand {
+	return map[string]cliCommand{
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
+		},
+		"help": {
+			name:        "help",
+			description: "Displays a help message",
+			callback:    commandHelp,
+		},
+	}
+}
+
+func commandExit() error {
+	fmt.Println("Closing the Pokedex... Goodbye!")
+	os.Exit(0)
+	return nil
+}
+
+func commandHelp() error {
+	fmt.Println("Welcome to the Pokedex!")
+	fmt.Println("Usage:")
+	registry := getCommand()
+
+	for key, value := range registry {
+		fmt.Printf("%s: %s\n", key, value.description)
+	}
+	return nil
 }
