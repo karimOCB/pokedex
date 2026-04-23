0e429fecc3b6d0015aefa48ed3b2faa95e49da68
Karim OCB <chaya.karim.omar@gmail.com>
2026-04-23 23:32:57 +0000

    AK: Testing 02

--- internal/pokecache/pokecache_test.go
+++ internal/pokecache/pokecache_test.go
@@ -1,6 +1,7 @@
 package pokecache
 
 import (
+	"fmt"
 	"testing"
 	"time"
 )
@@ -22,18 +23,89 @@ func TestAddGet(t *testing.T) {
 
 	cache := NewCache(1 * time.Second)
 
-	for _, c := range cases {
-		cache.Add(c.key, c.val)
-		val, ok := cache.Get(c.key)
-		if !ok {
-			t.Errorf("url %q is not cached", c.key)
-			return
-		}
+	for i, c := range cases {
+		t.Run(fmt.Sprintf("case_%d_%s", i, c.key), func(t *testing.T) {
+			cache.Add(c.key, c.val)
+			val, ok := cache.Get(c.key)
+			if !ok {
+				t.Errorf("url %q is not cached", c.key)
+				return
+			}
 
-		if string(val) != string(c.val) {
-			t.Errorf("url %q | Expected: %q | Got Value: %q", c.key, string(c.val), string(val))
-			return
-		}
+			if string(val) != string(c.val) {
+				t.Errorf("url %q | Expected: %q | Got Value: %q", c.key, string(c.val), string(val))
+				return
+			}
+		})
+	}
+}
 
+func TestReapLoop(t *testing.T) {
+	cases := []struct {
+		key string
+		val []byte
+	}{
+		{
+			key: "fakeurl.com",
+			val: []byte("example value"),
+		},
+		{
+			key: "newfake.com",
+			val: []byte("hello"),
+		},
+	}
+
+	for i, c := range cases {
+		t.Run(fmt.Sprintf("case: %d. URL: %q", i, c.key), func(t *testing.T) {
+			cache := NewCache(5 * time.Millisecond)
+			cache.Add(c.key, c.val)
+			val, ok := cache.Get(c.key)
+			if !ok {
+				t.Errorf("url %q is not cached", c.key)
+				return
+			}
+
+			if string(val) != string(c.val) {
+				t.Errorf("url %q | Expected: %q | Got Value: %q", c.key, string(c.val), string(val))
+				return
+			}
+
+			time.Sleep(10 * time.Millisecond)
+
+			_, ok = cache.Get(c.key)
+			if ok {
+				t.Errorf("url %q is cached", c.key)
+				return
+			}
+		})
+	}
+}
+
+func TestGet(t *testing.T) {
+	cases := []struct {
+		key string
+		ok  bool
+	}{
+		{
+			key: "fakeurl.com",
+			ok:  false,
+		},
+		{
+			key: "newfake.com",
+			ok:  false,
+		},
+	}
+
+	for i, c := range cases {
+		t.Run(fmt.Sprintf("case: %d | url: %q", i, c.key), func(t *testing.T) {
+			cache := NewCache(5 * time.Millisecond)
+
+			_, ok := cache.Get(c.key)
+
+			if ok != c.ok {
+				t.Errorf("url: %q should not hold a value", c.key)
+				return
+			}
+		})
 	}
 }
